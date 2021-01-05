2bf72ffc845e7fbb3166f232513160c25e33dc29
Austin Hernandez <ahernandez1170@gmail.com>
2021-01-05 23:40:08 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_numbers = []
   array.each do |num|
     new_numbers << num ** 2 
-    array.inject{|new_numbers, n| new_numbers ** 2}
+    array.collect{|new_numbers, n| new_numbers ** 2}
   end
   new_numbers
 end
